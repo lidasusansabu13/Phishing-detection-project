1fb4fd1ff3c3901105777fb9322cc3d88b25f929
Lida Susan Sabu <lidasusansabu13@gmail.com>
2019-04-11 15:40:19 +0530

    Update labeling.py

--- labeling.py
+++ labeling.py
@@ -7,6 +7,7 @@
 # 5/30.............................status
 import requests
 from bs4 import BeautifulSoup
+import re
 '''
 import requests 
 
@@ -25,11 +26,20 @@ url='https://www.ktustudy.in/computer-science-s8-syllabus'
 
 
 labels=[0]*30
-#1.having IP address
+
 url_tokens='/'.join(url.split('//')).split('/')
 print url_tokens
-domain=url_tokens[1]
-having_IP=0
+#1.having IP address
+match=re.search('(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\/)|'  #IPv4
+                    '((0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\/)'  #IPv4 in hexadecimal
+                    '(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}',url)     #Ipv6
+if match:
+    #print match.group()
+    having_IP= 1            # phishing
+else:
+    #print 'No matching pattern found'
+    having_IP= -1           # legitimate
+
 
 #2.URL length
 length=len(url)
@@ -40,7 +50,18 @@ elif length>=54 and length<75:
 else:
     url_len=1
 #3.URL shortening services
-shortining=0
+matchs=re.search('bit\.ly|goo\.gl|shorte\.st|go2l\.ink|x\.co|ow\.ly|t\.co|tinyurl|tr\.im|is\.gd|cli\.gs|' 
+                    'yfrog\.com|migre\.me|ff\.im|tiny\.cc|url4\.eu|twit\.ac|su\.pr|twurl\.nl|snipurl\.com|' 
+                    'short\.to|BudURL\.com|ping\.fm|post\.ly|Just\.as|bkite\.com|snipr\.com|fic\.kr|loopt\.us|'
+                    'doiop\.com|short\.ie|kl\.am|wp\.me|rubyurl\.com|om\.ly|to\.ly|bit\.do|t\.co|lnkd\.in|'
+                    'db\.tt|qr\.ae|adf\.ly|goo\.gl|bitly\.com|cur\.lv|tinyurl\.com|ow\.ly|bit\.ly|ity\.im|'
+                    'q\.gs|is\.gd|po\.st|bc\.vc|twitthis\.com|u\.to|j\.mp|buzurl\.com|cutt\.us|u\.bb|yourls\.org|'
+                    'x\.co|prettylinkpro\.com|scrnch\.me|filoops\.info|vzturl\.com|qr\.net|1url\.com|tweez\.me|v\.gd|tr\.im|link\.zip\.net',url)
+if matchs:
+    shortining=1    # phishing
+else:
+    shortining=-1    # legitimate
+    
 #4.URL is having ar symbol
 if '@' in url:
     having_at=1
@@ -60,7 +81,13 @@ if '-' in url_tokens[1]:
 else:
     prefix_suffix=-1
 #7.Sub Domain and Multi Sub Domains
-having_Sub_Domain=0
+if url.count(".") < 3:
+    having_Sub_Domain= -1   # legitimate
+elif url.count(".") == 3:
+    having_Sub_Domain=0     # suspicious
+else:
+    having_Sub_Domain=1     # phishing
+
 #8.HTTPS (Hyper Text Transfer Protocol with Secure Sockets Layer)
 if(url_tokens[0]=='https:'):
     sSLfinal_State=-1
